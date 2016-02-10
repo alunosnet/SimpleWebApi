04ee2ec921a0c3d8f916eb34aed6107a43d05106
alunosnet <alunosnet@gmail.com>
2016-02-10 15:26:45 +0000

    Projeto simples de como fazer uma página aspx que é chamada a partir de uma aplicação desktop

---
+++ formweb_servidor/inventario.aspx
@@ -0,0 +1,2 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="inventario.aspx.cs" Inherits="formweb_servidor.inventario" %>
+
---
+++ formweb_servidor/inventario.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace formweb_servidor
+{
+    public partial class inventario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string p1;
+            if (!IsPostBack)
+            {
+                if (Request.Params != null && Request.Params[0] != null)
+                {
+                    p1 = Request.Params[0].ToString();
+                    Response.Write(p1);
+                }
+            }
+        }
+    }
+}
